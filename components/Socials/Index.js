a190a4d1d3f99a983b332d348b32cd5d2ab99d9c
UsamaKashif <usamakashif1@gmail.com>
2021-09-04 23:15:11 +0500

    link fix

--- components/Socials/Index.js
+++ components/Socials/Index.js
@@ -3,19 +3,19 @@ import styles from "./Socials.module.css"
 const Socials = () => {
     return (
         <div className={styles.socials}>
-            <a href="https://www.instagram.com/usama_codes/" target="_blank">
+            <a href="https://www.instagram.com/usama_codes/" rel="noreferrer" target="_blank">
                 <img src="/icons/instagram.svg" alt="" />
             </a>
-            <a href="https://github.com/UsamaKashif/" target="_blank">
+            <a href="https://github.com/UsamaKashif/" rel="noreferrer" target="_blank">
                 <img src="/icons/github.svg" alt="" />
             </a>
-            <a href="https://dribbble.com/usamaKashif" target="_blank">
+            <a href="https://dribbble.com/usamaKashif" rel="noreferrer" target="_blank">
                 <img src="/icons/dribbble.svg" alt="" />
             </a>
-            <a href="https://www.behance.net/usamakashif" target="_blank">
+            <a href="https://www.behance.net/usamakashif" rel="noreferrer" target="_blank">
                 <img src="/icons/behance.svg" alt="" />
             </a>
-            <a href="https://www.fiverr.com/usama_kashif" target="_blank">
+            <a href="https://www.fiverr.com/usama_kashif" rel="noreferrer" target="_blank">
                 <img src="/icons/fiverr.svg" alt="" />
             </a>
         </div>
